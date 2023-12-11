f4fa07f8fb670af4ed2237bc2da6a896fa902a4d
Amappola <anam.porrasp@gmail.com>
2023-12-11 15:45:27 -0500

    Setting up routes for lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./ui/pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'shopping-cart',
+    loadChildren: () => import('./ui/pages/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./ui/pages/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./ui/pages/admin/admin.module').then(m => m.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
