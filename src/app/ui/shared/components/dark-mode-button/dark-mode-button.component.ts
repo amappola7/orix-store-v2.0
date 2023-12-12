904d41791b06e8a086f65410d34dd0916e854429
Amappola <anam.porrasp@gmail.com>
2023-12-12 14:56:06 -0500

    Creating the dark mode component

---
+++ src/app/ui/shared/components/dark-mode-button/dark-mode-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'orix-dark-mode-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './dark-mode-button.component.html',
+  styleUrl: './dark-mode-button.component.scss'
+})
+export class DarkModeButtonComponent {
+
+}
