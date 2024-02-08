18cdc2b4e5bf42b68392d21a8a7596183ed4ae10
Amappola <anam.porrasp@gmail.com>
2024-02-08 15:00:22 -0500

    Creating product list

--- src/app/ui/pages/home/components/product-list/product-list-page/product-list-page.component.ts
+++ src/app/ui/pages/home/components/product-list/product-list-page/product-list-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductM } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'orix-product-list-page',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './product-list-page.component.scss'
 })
 export class ProductListPageComponent {
+  productsList: ProductM[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts()
+    .subscribe((products) => {
+      this.productsList = products;
+    })
+  }
 }
