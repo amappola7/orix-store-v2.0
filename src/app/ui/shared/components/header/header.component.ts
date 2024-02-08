4821ce6a8c62f205fe96c1c9927fc9ff1a41c75d
Amappola <anam.porrasp@gmail.com>
2024-02-08 14:31:36 -0500

    Creating header layout

--- src/app/ui/shared/components/header/header.component.ts
+++ src/app/ui/shared/components/header/header.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'orix-header',
-  standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- src/app/ui/shared/shared.module.ts
+++ src/app/ui/shared/shared.module.ts
@@ -6,17 +6,22 @@ import { StoreModule } from '@ngrx/store';
 import { darkModeReducer } from '../../services/dark-mode/dark-mode.reducer';
 import { DarkModeButtonComponent } from './components/dark-mode-button/dark-mode-button.component';
 import { SharedRoutingModule } from './shared-routing.module';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    DarkModeButtonComponent
+    DarkModeButtonComponent,
+    NavBarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     FontAwesomeModule,
-    // StoreModule.forRoot({screenMode: darkModeReducer}),
     SharedRoutingModule
   ],
   exports: [
