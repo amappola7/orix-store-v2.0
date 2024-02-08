1c906b1620d0655bba4d1713f9b575436f40b281
Amappola <anam.porrasp@gmail.com>
2024-02-08 09:41:55 -0500

    Refactoring routing modules

---
+++ src/app/app-routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from "@angular/router";
+
+export const appRoutes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./ui/pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'shopping-cart',
+    loadChildren: () => import('./ui/pages/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./ui/pages/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./ui/pages/admin/admin.module').then(m => m.AdminModule)
+  }
+];
