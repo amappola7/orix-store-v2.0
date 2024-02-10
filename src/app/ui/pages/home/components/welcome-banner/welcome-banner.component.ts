89a3a17f8a037dc7f2a191fb12b8aef90bcfb7a7
Amappola <anam.porrasp@gmail.com>
2024-02-10 13:49:51 -0500

    Adding styles

--- src/app/ui/pages/home/components/welcome-banner/welcome-banner.component.ts
+++ src/app/ui/pages/home/components/welcome-banner/welcome-banner.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faAnglesDown, faArrowDownLong } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'orix-welcome-banner',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './welcome-banner.component.scss'
 })
 export class WelcomeBannerComponent {
+  arrowIcon: IconDefinition = faAnglesDown;
   screenSize: number = window.screen.width;
 }
