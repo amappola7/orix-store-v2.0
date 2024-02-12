802188a5f67476c10949096fd45d4671b47c4a52
Amappola <anam.porrasp@gmail.com>
2024-02-12 08:25:46 -0500

    Fixing styles

--- src/app/ui/shared/components/header/header.component.ts
+++ src/app/ui/shared/components/header/header.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'orix-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss', './header-desktop.component.scss']
 })
 export class HeaderComponent {
   iconMenu: IconDefinition = faBars;
   screenSize: number = window.screen.width;
   screenMode: boolean = true;
+  logoToUse!: string;
   logos = {
     lightModeLogo: '../../../../../assets/logos/Logo Light Mode.png',
     darkModeLogo: '../../../../../assets/logos/Logo Dark Mode.png',
+  }
+  @Input() currentPage!: string;
+
+  settingLogo(): void {
+    if (this.screenSize > 768 && this.currentPage == 'home') {
+      this.logoToUse = this.logos.darkModeLogo;
+    } else if (this.screenMode) {
+      this.logoToUse = this.logos.lightModeLogo;
+    } else {
+      this.logoToUse = this.logos.darkModeLogo;
+    }
+  }
+
+  ngOnInit() {
+    this.settingLogo();
   }
 }
