058b17e84d56daa63c9561cf814fd1861574c250
Amappola <anam.porrasp@gmail.com>
2024-02-12 11:42:06 -0500

    Setting up routing for product details

--- src/app/ui/pages/home/home-routing.module.ts
+++ src/app/ui/pages/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home.component";
+import { ProductDetailsPageComponent } from "./components/product-details/product-details-page/product-details-page.component";
 
 const homeRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'details/:id',
+    component: ProductDetailsPageComponent
   }
 ];
 
