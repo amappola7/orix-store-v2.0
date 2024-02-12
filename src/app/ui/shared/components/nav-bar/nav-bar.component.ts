37a729a76a7bd2576ebc7b0e61b9ae936a431136
Amappola <anam.porrasp@gmail.com>
2024-02-12 07:33:14 -0500

    Defining nav bar desktop styles

--- src/app/ui/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/ui/shared/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'orix-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrls: ['./nav-bar-desktop.component.scss', './nav-bar.component.scss']
 })
 export class NavBarComponent {
-
+  cartIcon: IconDefinition = faShoppingCart
 }
