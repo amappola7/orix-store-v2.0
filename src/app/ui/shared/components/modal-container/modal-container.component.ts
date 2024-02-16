3613b8dbdf108dbe2bb7f2702b4e6134a90bec8f
Amappola <anam.porrasp@gmail.com>
2024-02-16 12:36:27 -0500

    Adding product form to modal

--- src/app/ui/shared/components/modal-container/modal-container.component.ts
+++ src/app/ui/shared/components/modal-container/modal-container.component.ts
@@ -1,22 +1,16 @@
-import { Component, EventEmitter, Input, Output, ContentChildren, QueryList } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ContentChildren, QueryList, ViewContainerRef, OnInit } from '@angular/core';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { DarkModeButtonComponent } from '../dark-mode-button/dark-mode-button.component';
-import { ProductFormComponent } from 'src/app/ui/pages/admin/components/product-form/product-form.component';
 
 @Component({
   selector: 'orix-modal-container',
   templateUrl: './modal-container.component.html',
   styleUrls: ['./modal-container.component.scss', './modal-container-desktop.component.scss']
 })
-export class ModalContainerComponent /*implements OnInit, OnDestroy*/ {
+export class ModalContainerComponent {
   closeModalIcon: IconDefinition = faXmark;
 
   @Output() closeModalNotification: EventEmitter<string> = new EventEmitter();
-
-  @ContentChildren('modalContent') contentComponents!: QueryList<ProductFormComponent>;
-
-  constructor (  ) {}
 
   closeModal(): void {
     this.closeModalNotification.emit('close');
--- src/app/ui/shared/components/modal-container/modalPossibleContents.ts
+++
@@ -1,5 +0,0 @@
-import { ProductFormComponent } from "src/app/ui/pages/admin/components/product-form/product-form.component";
-
-export const modalPossibleContents = {
-  productForm: ProductFormComponent
-}
