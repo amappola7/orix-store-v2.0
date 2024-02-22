7b54c8204be91f374c8609ffe710a40090c340d6
Amappola <anam.porrasp@gmail.com>
2024-02-22 11:33:13 -0500

    Adding modo oscuro

--- src/app/ui/shared/components/footer/footer.component.ts
+++ src/app/ui/shared/components/footer/footer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'orix-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  screenMode$!: Observable<boolean>;
+  screenMode!: boolean;
 
+  constructor(
+    private store: Store<{screenMode: boolean}>
+  ) {}
+
+  ngOnInit(): void {
+    this.screenMode$ = this.store.select('screenMode');
+    this.screenMode$.subscribe(mode => this.screenMode = mode);
+  }
+
+  screenModeChange(): void {
+    this.screenMode$.subscribe(mode => this.screenMode = mode);
+  }
 }
