d6e59d45cd9ad5188b2a7292b2352782c4f3b645
Amappola <anam.porrasp@gmail.com>
2024-02-22 21:12:30 -0500

    Finishing cart

--- src/app/ui/pages/home/components/product-details/product-details-card/product-details-card.component.ts
+++ src/app/ui/pages/home/components/product-details/product-details-card/product-details-card.component.ts
@@ -3,7 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { faMinus, faPlus, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { Observable } from 'rxjs';
 import { ProductM } from 'src/app/models/product';
+import { ShoppingCartItemM } from 'src/app/models/shopping-cart';
 import { ProductService } from 'src/app/services/product/product.service';
+import { ShoppingCartService } from 'src/app/services/shopping-cart/shopping-cart.service';
 
 @Component({
   selector: 'orix-product-details-card',
@@ -17,20 +19,47 @@ export class ProductDetailsCardComponent implements OnInit {
     add: faPlus,
     remove: faMinus,
     shoppingCart: faShoppingCart
-  }
+  };
+  cart: ShoppingCartItemM[] = [];
+  totalCart: number = 0;
+  quantity: number = 1;
 
   constructor(
     private productService: ProductService,
+    private shoppingCartService: ShoppingCartService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.route.params
       .subscribe((params) => {
-        console.log(params);
         this.productId = params['id'];
         this.productInfo$ = this.productService.getProductById(this.productId);
       });
+
+    this.cart = this.shoppingCartService.cart;
   }
+
+  addQuantityToProduct(): void {
+    this.quantity += 1;
+  }
+
+  removeQuantityToProduct(): void {
+    if (this.quantity > 0) this.quantity -= 1;
+  }
+
+  // openCartAlert(): void {
+  //   this._snackBar.open('Product added to cart', '', {
+  //     duration: 2500,
+  //   });
+  // }
+
+  addProductToCart(id: number): void {
+    this.productService.getProductById(id)
+    .subscribe((product) => {
+      this.shoppingCartService.addProduct(product!, this.quantity);
+      // this.openCartAlert();
+    })
+  };
 }
 
--- src/app/ui/pages/home/components/product-list/product-list-card/product-list-card.component.ts
+++ src/app/ui/pages/home/components/product-list/product-list-card/product-list-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { faCircleInfo, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { ProductM } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product/product.service';
+import { ShoppingCartService } from 'src/app/services/shopping-cart/shopping-cart.service';
 
 @Component({
   selector: 'orix-product-list-card',
@@ -12,5 +14,25 @@ export class ProductListCardComponent {
     shoppingCartIcon: faShoppingCart,
     moreInfoIcon: faCircleInfo
   };
+
   @Input() productInfo!: ProductM;
+
+  constructor(
+    private productService: ProductService,
+    private shoppingCartService: ShoppingCartService
+  ) { }
+
+  // openCartAlert(): void {
+  //   this._snackBar.open('Product added to cart', '', {
+  //     duration: 2500,
+  //   });
+  // }
+
+  addProductToCart(id: number): void {
+    this.productService.getProductById(id)
+      .subscribe((product) => {
+        this.shoppingCartService.addProduct(product!, 1);
+        // this.openCartAlert();
+      })
+  }
 }
