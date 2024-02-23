446f60fa49868f858735df60c2b02788102dd85a
Amappola <anam.porrasp@gmail.com>
2024-02-23 10:04:05 -0500

    Card dark mode

--- src/app/ui/pages/home/components/product-list/product-list-card/product-list-card.component.ts
+++ src/app/ui/pages/home/components/product-list/product-list-card/product-list-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { faCircleInfo, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { ProductM } from 'src/app/models/product';
 import { AlertsService } from 'src/app/services/alerts/alerts.service';
 import { ProductService } from 'src/app/services/product/product.service';
@@ -16,20 +18,28 @@ export class ProductListCardComponent {
     moreInfoIcon: faCircleInfo
   };
 
+  screenMode$!: Observable<boolean>;
+  screenMode!: boolean;
+
   @Input() productInfo!: ProductM;
 
   constructor(
     private productService: ProductService,
     private shoppingCartService: ShoppingCartService,
-    private alertsService: AlertsService
+    private alertsService: AlertsService,
+    private store: Store<{ screenMode: boolean }>
   ) { }
+
+  ngOnInit(): void {
+    this.screenMode$ = this.store.select('screenMode');
+    this.screenMode$.subscribe(mode => this.screenMode = mode);
+  }
 
   addProductToCart(id: number): void {
     this.productService.getProductById(id)
       .subscribe((product) => {
         this.shoppingCartService.addProduct(product!, 1);
         this.alertsService.showSimpleAlert('Product added to cart', 'Done');
-        // this.openCartAlert();
       })
   }
 }
