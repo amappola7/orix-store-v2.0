12a8b5a20c405112d2da1af74a2eb8336633c4c2
Amappola <anam.porrasp@gmail.com>
2024-02-26 09:24:39 -0500

    Details dark mode

--- src/app/ui/pages/home/components/product-details/product-details-page/product-details-page.component.ts
+++ src/app/ui/pages/home/components/product-details/product-details-page/product-details-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'orix-product-details-page',
@@ -8,6 +10,17 @@ import { Component } from '@angular/core';
 export class ProductDetailsPageComponent {
   displayMobileMenu: boolean = false;
   screenSize: number = window.screen.width;
+  screenMode$!: Observable<boolean>;
+  screenMode!: boolean;
+
+  constructor(
+    private store: Store<{ screenMode: boolean }>
+  ) { }
+
+  ngOnInit(): void {
+    this.screenMode$ = this.store.select('screenMode');
+    this.screenMode$.subscribe(mode => this.screenMode = mode);
+  }
 
   openMobileMenu(): void {
     this.displayMobileMenu = true;
