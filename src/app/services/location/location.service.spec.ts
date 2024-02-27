5fa9d0c8f6f895a4c507f6120931aa5daf509e0c
Amappola <anam.porrasp@gmail.com>
2024-02-27 12:43:08 -0500

    Providing the HTTP client module to testing bed

--- src/app/services/location/location.service.spec.ts
+++ src/app/services/location/location.service.spec.ts
@@ -1,13 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController }  from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { LocationService } from './location.service';
 
 describe('LocationService', () => {
   let service: LocationService;
-
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(LocationService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
     service = TestBed.inject(UserService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
